81a956f70e90843141277767a22801649914e543
Eljan Simuratli <44237641+Simuratli@users.noreply.github.com>
2026-05-06 16:10:20 +0400

    font implementation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,15 @@
 import { Link, Stack } from "expo-router";
 import { styled } from "nativewind";
+import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
 const Home = () => {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-7xl font-sans-extrabold">Home</Text>
+      <Text className="text-7xl font-bold">Home</Text>
+
       <Stack />
       <Link href="/(auth)/SignIn">Go to Sign In</Link>
       <Link href="/(tabs)/settings">Go to Settings</Link>
